0476d245e5a3d5aad0ad883f1b2697eebea91eba
Joshua Wierenga <joshua.wierenga.student@gmail.com>
2018-09-10 22:31:51 +1000

    Created base cellular automata class
    Added 2d bit array

---
+++ CellularAutomata/CellularAutomata.cs
@@ -0,0 +1,13 @@
+﻿namespace CellularAutomata
+{
+    public abstract class CellularAutomata
+    {
+        public uint CurrentRow { get; protected set; }
+
+        public BitMatrix State { get; protected set; }
+
+        public abstract void Iterate();
+
+        public abstract void Draw();
+    }
+}
